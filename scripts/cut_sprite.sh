8d39c2da17786e0132ea60eb3720af6779864ba4
Manuel Luna <manuellunada@gmail.com>
2025-08-16 15:14:04 -0600

    sleeping and idle cat sprites

--- scripts/cut_sprite.sh
+++ scripts/cut_sprite.sh
@@ -1,23 +1,27 @@
 #! /usr/bin/env bash
 
-name="$1"
-name_ext="${name##*.}"
-size="${2:-32x32}"
+file="$1"
+file_ext="${file##*.}"
+name="$2"
+animation="$3"
+size="${4:-32x32}"
 
 usage() {
   cat <<EOF
 Usage:
 
 # Put your raw sprite in assets/ and run
-$ cut_sprite.sh <sprite_name> [<size>]
+$ cut_sprite.sh <sprite_file> <sprite_name> <animation_name> [<size>]
 
 # With a specific size (default: 32x32)
-$ cut_sprite.sh cute_and_big_bunny.png 64x64
+$ cut_sprite.sh cute_and_big_bunny.png big_bunny sleeping 64x64
 EOF
   exit 1
 }
 
+[ -z "$file" ] && usage
 [ -z "$name" ] && usage
+[ -z "$animation" ] && usage
 
 if [ -d "./sprites" ]; then
   cd sprites || exit
@@ -25,20 +29,20 @@ else
   cd ../sprites || exit
 fi
 
-if [ ! -r "../assets/$name" ]; then
-  echo "$name sprite not found"
+if [ ! -r "../assets/$file" ]; then
+  echo "$file sprite not found"
   exit 1
 fi
 
-sprite_dir="$(basename "$name" ".$name_ext")"
+sprite_dir="$name"
 mkdir -p "$sprite_dir" || exit
 cd "$sprite_dir" || exit
 
-magick "../../assets/$name" -crop "$size" "tile.png"
+magick "../../assets/$file" -crop "$size" "tile.png"
 
 for tile in tile*.png; do
-  tile_no_ext=$(basename $tile .png)
-  chafa --size "$size" "$tile" | tee "${tile_no_ext}.ascii"
+  filename=$(sed "s|tile|$animation|" <<< "$tile")
+  chafa --size "$size" "$tile" | tee "$filename"
 done
 
 rm tile*.png
